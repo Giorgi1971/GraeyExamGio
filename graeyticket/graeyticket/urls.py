f6071ff0bfa5506801d1d961406252d63c6febef
Giorgi1971 <giooorgi7@gmail.com>
2022-07-07 18:26:06 +0400

    begin to complate all tasks

--- graeyticket/graeyticket/urls.py
+++ graeyticket/graeyticket/urls.py
@@ -10,7 +10,7 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
-    # my_urls in wellwash app
+    # my_urls in well_wash app
     path('', include('tickets.urls', namespace='tickets')),
     path('', include('user.urls', namespace='user')),
 ]
